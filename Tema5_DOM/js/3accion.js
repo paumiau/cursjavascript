7343fd5c4684e97297c29b0773d635487e7f9b7c
Pau Gonzalez <paumiau@gmail.com>
2020-07-30 16:24:13 +0200

    ejercicio4

---
+++ Tema5_DOM/js/3accion.js
@@ -0,0 +1,33 @@
+function crecer(){
+    pika.width=300;
+    pika.height=300;
+}
+function decrecer(){
+    pika.width=200;
+    pika.height=200;           
+}
+function mensaje(){
+    alert("Hola Soy Pikachu!")
+}
+function mensaje2(){
+    alert("Hola Soy tu padre")
+}
+
+//METODO 2
+
+function inicializa(){
+    var pika=document.getElementById("pika");
+    pika.onmouseover=crecer;
+    pika.onmouseout=decrecer;
+    pika.onclick=mensaje;
+}
+
+//METODO 3
+function inicializa(){
+    var pika=document.getElementById("pika");
+    pika.addEventListener("mouseover",crecer);
+    pika.addEventListener("mouseout",decrecer);
+    pika.addEventListener("click",mensaje);
+    pika.addEventListener("click",mensaje2);
+}
+window.onload=inicializa;
